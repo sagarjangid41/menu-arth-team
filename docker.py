9162fb68ebd2edc042c23a5dcbd12568cc0b0299
Sagar Jangid <60397881+sagarjangid41@users.noreply.github.com>
2020-11-01 18:24:06 +0530

    Update docker.py

--- docker.py
+++ docker.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 os.system("clear")
 os.system("tput setaf 3")
 print("\t\t\tLocal > docker")
@@ -18,20 +19,29 @@ while True:
 	cmd=int(input("Enter your choice \t"))
 	if cmd==1:
 		print("docker ps")
+		print(subprocess.getoutput("sudo docker ps"))
 	elif cmd==2:
 		print("docker ps -a")
+		print(subprocess.getoutput("sudo docker ps -a"))
 	elif cmd==3:
 		print("docker images")
+		print(subprocess.getoutput("sudo docker images"))
 	elif cmd==4:
 		name=input("Enter container name\t")
 		image=input("Enter container image name\t")
 		print("docker run -it --name {} {}".format(name,image))
+		cmd1="docker run -it --name {} {}".format(name,image)
+		print(subprocess.getoutput("sudo cmd1"))
 	elif cmd==5:
 		sname=input("Enter container name\t")
 		print("docker stop {}".format(sname))
+		cmd2="docker stop {}".format(sname)
+		print(subprocess.getoutput("sudo cmd2"))
 	elif cmd==6:
 		aname=input("Enter Container name\t")
 		print("docker attach {}".format(aname))
+		cmd3="docker attach {}".format(aname)
+		print(subprocess.getoutput("sudo cmd4"))
 	elif cmd==7:
 		exit()
 
